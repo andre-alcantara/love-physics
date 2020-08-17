00197e7ab8b6f49d420b15a002fbe5c2f77ba8aa
André Pimentel <68190715+andre-cefiroti@users.noreply.github.com>
2020-08-17 05:37:39 -0300

    Finishing Welcome Screen

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,24 +1,59 @@
 import React, { useState } from 'react';
 import { View, Text, FlatList, SafeAreaView, Image, TouchableOpacity, TouchableOpacityBase } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import ViewPager from '@react-native-community/viewpager';
+import Swiper from 'react-native-swiper';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
-import WelcomeList from '../../components/WelcomeList'
-// import { Container } from './styles';
+import { Wrapper, Container, Slide, Title, SignInButton, SignInText, SignUpButton, SignUpText } from './styles'
+
+import Img1 from '../../img/img1.svg';
+import Img2 from '../../img/img2.svg';
+import Img3 from '../../img/img3.svg';
 
 export default function Welcome() {
+
+
   return (
-      <SafeAreaView style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#FFF'}}>
-  
-        <StatusBar style='dark' />
-        <View style={{
-          width: wp('100%'),
-          height: hp('100%')
-        }}>
-        <WelcomeList />
-      
-        </View>
-        </SafeAreaView>
+    <Wrapper>
+
+      <StatusBar style='auto' />
+      <Container>    
+        <Swiper autoplayTimeout={4} 
+          autoplay={true} 
+          autoplayDirection={true} 
+          showsButtons={false} 
+          dotStyle={{backgroundColor: '#C4C4C4', width: 7, height: 7, marginBottom: hp('-4%'), marginTop: hp('4%')}} 
+          activeDotStyle={{width: 9, height: 9, marginBottom: hp('-4%'), marginTop: hp('4%')}}
+          activeDotColor='#FF5555'>
+
+          <Slide>
+            <Title>Aprenda</Title>
+            <Img1 width={wp('100%')} height={hp('50%')} />
+          </Slide>
+            
+          <Slide>
+            <Title>Avance</Title>
+            <Img2 width={wp('100%')} height={hp('50%')} />
+          </Slide>
+
+          <Slide>
+            <Title>Conquiste</Title>
+            <Img3 width={wp('100%')} height={hp('50%')} />
+          </Slide>
+        </Swiper>
+
+        </Container>
+        
+
+        <SignInButton>
+          <SignInText>Entrar</SignInText>
+        </SignInButton>
+
+        <SignUpButton>
+          <SignUpText>Criar Conta</SignUpText>
+        </SignUpButton>
+        </Wrapper>
   );
 }   
+
+console.disableYellowBox = true;
--- src/pages/Welcome/styles.js
+++ src/pages/Welcome/styles.js
@@ -1,1 +1,62 @@
 import styled from 'styled-components';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
+export const Wrapper = styled.SafeAreaView `
+    flex: 1;
+    justify-content: center; 
+    align-items: center; 
+    background-color: #FFF;
+`;
+
+export const Container = styled.View `
+    width: ${wp('100%')}px;
+    height: ${hp('70%')}px;
+    margin-top: ${hp('-15%')}px;
+    background-color: #FFF;
+`;
+
+export const Slide = styled.View `
+    height: 65%; 
+    margin-top: ${hp('6%')}px;
+`;
+
+export const Title = styled.Text `
+    text-align: center;
+    margin-bottom: ${hp('4%')}px;
+    font-size: ${hp('5%')}px;
+    font-family: Montserrat_700Bold;
+`;
+
+export const SignInButton = styled.TouchableOpacity `
+    margin-top: ${hp('6%')};
+    margin-bottom: ${hp('-6%')};
+    background-color: #FF5555;
+    width: 60%;
+    border-radius: 15px;
+`;
+
+export const SignInText = styled.Text `
+    font-size: 16px;
+    padding: 15px;
+    text-align: center;
+    color: #FFF;
+    font-family: Montserrat_600SemiBold;
+`;
+
+export const SignUpButton = styled.TouchableOpacity `
+    margin-top: ${hp('7%')};
+    margin-bottom: ${hp('-7%')};
+    background-color: #FFF;
+    width: 60%;
+    border-radius: 15px;
+    border-width: 2px;
+    border-color: #FF5555; 
+`;
+
+export const SignUpText = styled.Text `
+    font-size: 16px;
+    padding: 13px;
+    color: #FF5555;
+    text-align: center;
+    font-family: Montserrat_600SemiBold;
+`;
